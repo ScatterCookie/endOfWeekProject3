6db5a675be5d518097d90556553ab77279d8bcdc
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-22 15:21:25 +0000

    main sections of functions and css done

---
+++ app.js
@@ -0,0 +1,33 @@
+//what the initial value of cookies and current cookies are and an array with the current purchases in.
+let initialState = {
+  cookies: 0,
+  purchases: [],
+  currentCookies: 0,
+};
+
+let currentCookies = initialState.currentCookies;
+
+const cookieCounter = document.getElementById("cookiePerSecond");
+const cps = document.getElementById("cps");
+const shop = document.getElementById("shop");
+const centerCookie = document.getElementById("center-cookie");
+
+const game = function () {};
+
+centerCookie.addEventListener("click", function () {});
+
+function getCPS() {}
+
+async function getUpgrades() {}
+
+function openShop(params) {}
+
+function buyItems() {}
+
+function updateUI(initialState) {}
+
+function saveGame() {}
+
+function loadGame() {}
+
+game();
